6a4fbf8f2b1fef7d1fe782b24957e66b4e8ea08e
Jan Pomikalek <jan.pomikalek@gmail.com>
2018-10-23 08:16:06 +0200

    replace Foobar in the license text :)

--- vega.ino
+++ vega.ino
@@ -1,3 +1,22 @@
+/*
+    Copyright 2018 Jan Pomikalek <jan.pomikalek@gmail.com>
+
+    This file is part of the VeGa firmware.
+
+    VeGa firmware is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    VeGa firmware is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with VeGa firmware.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "vega_display.h"
 #include "vega_eeprom.h"
 #include "vesc_comm.h"
