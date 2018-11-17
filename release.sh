01398ae2a77fc0267596c3a36e14614db592991a
Jan Pomikalek <jan.pomikalek@gmail.com>
2018-11-17 22:39:29 +0100

    improved release script

--- release.sh
+++ release.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 VERSION=$(grep FW_VERSION davega.ino | cut -d' ' -f3 | tr -d '"')
-RELEASE_FILEPATH="dist/davega-$VERSION.zip"
+RELEASE_DIR="davega-$VERSION"
+RELEASE_FILE="davega-$VERSION.zip"
+
 mkdir -p dist/
-echo "Creating $RELEASE_FILEPATH"
-rm -f ${RELEASE_FILEPATH}
-zip ${RELEASE_FILEPATH} *.ino *.cpp *.h LICENSE README.md
+
+rm -f dist/${RELEASE_DIR}/* dist/${RELEASE_FILE}
+mkdir -p dist/${RELEASE_DIR}/
+
+echo "Copying files to dist/$RELEASE_DIR"
+cp *.ino *.cpp *.h LICENSE README.md dist/${RELEASE_DIR}
+mv dist/${RELEASE_DIR}/davega.ino dist/${RELEASE_DIR}/davega-${VERSION}.ino
+
+echo "Creating dist/$RELEASE_FILE"
+cd dist/
+zip -r ${RELEASE_FILE} ${RELEASE_DIR}
